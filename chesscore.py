c6c4e24be2905e8d5e10c157b29226672922dea3
MaximeLT <34684770+MaximeLT@users.noreply.github.com>
2018-10-17 15:38:22 +0200

    Update chesscore.py

--- chesscore.py
+++ chesscore.py
@@ -1,4 +1,19 @@
-rien pour le moment
-ceci est joseph
+class Board :
+    arrayX = [1,2,3,4,5,6,7,8]
+    arrayY = [1,2,3,4,5,6,7,8]
+    def createBoard(arrayX, arrayY):
+        pass
 
-chui pas sur
+class Piece :
+
+class Pawn(Piece) :
+
+class King(Piece) :
+
+class Queen(Piece) :
+
+class Bishop(Piece) :
+
+class Knight(Piece) :
+
+class Rook(Piece) :
