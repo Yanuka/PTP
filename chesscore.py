816e7daec1682125bdaed3cef154ab1c73f50cb7
Nuddle <34684697+Nuddle@users.noreply.github.com>
2018-10-25 21:03:33 +0200

    swapped the order of the board display

--- chesscore.py
+++ chesscore.py
@@ -5,13 +5,14 @@ class Board(object) :
         for i in range(0,8):
             self.currentBoard += [[]]
             for j in range(0,8):
-                self.currentBoard[i] += [' ']
+                self.currentBoard[i] += [(i,j)]
 
     def draw(self):
-        for i in self.currentBoard:
+        for i in range(len(self.currentBoard)):
             count=0
             print("————————————————————————————————————————————————")
-            for j in i:
+            a = self.currentBoard[len(self.currentBoard)-i-1]
+            for j in a:
                 count+=1
                 print("| " , j , " ", end='')
                 if count == 8:
