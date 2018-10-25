0dc3672f39529aa7c043745f40e2fd56d0b74e0f
Joseph <lloyd.joseph89@gmail.com>
2018-10-25 14:07:14 +0200

    Loop start

--- chessgame.py
+++ chessgame.py
@@ -2,9 +2,12 @@ from chesscore import *
 
 board = Board()
 actualBoard = board.initialize()
-board.draw(actualBoard)
 
 print("Hello and welcome to Chess !")
-print("Select the piece you want to move and enter the coordinates to where you want to move it to")
-coordX = input()
-coordY = input()
+
+while :
+
+    board.draw(actualBoard)
+    print("Select the piece you want to move and enter the coordinates to where you want to move it to")
+    coordX = input()
+    coordY = input()
