664deda91fbbafcc95e6072529a050d62c7f2cf2
Joseph <lloyd.joseph89@gmail.com>
2018-10-25 15:37:00 +0200

    Corrected display 2

--- chessgame.py
+++ chessgame.py
@@ -14,10 +14,10 @@ while end != True:
         print("Player 2's turn")
     board.draw()
 
-    selectedPieceX = input("Select the X coordinates of the piece you want to move : ")
-    selectedPieceY = input("Select the Y coordinates of the piece you want to move : ")
-    selectedCoordX = input("Enter the X coordinates to where you want to move it to : ")
-    selectedCoordY = input("Enter the Y coordinates to where you want to move it to : ")
+    selectedPieceX = input("Select the X coordinate of the piece you want to move : ")
+    selectedPieceY = input("Select the Y coordinate of the piece you want to move : ")
+    selectedCoordX = input("Enter the X coordinate to where you want to move it to : ")
+    selectedCoordY = input("Enter the Y coordinate to where you want to move it to : ")
 
     print("\033[H\033[J")
 
