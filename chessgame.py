f72ec1d4994578bb20ff4437f0cdb0cd094d623d
Joseph <lloyd.joseph89@gmail.com>
2018-10-25 14:58:51 +0200

    Correct board display

--- chessgame.py
+++ chessgame.py
@@ -2,7 +2,6 @@ from chesscore import *
 import os
 
 board = Board()
-actualBoard = board.initialize()
 
 print("Hello and welcome to Chess !")
 currentPlayerIsOne = True
@@ -13,7 +12,7 @@ while end != True:
         print("Player 1's turn")
     else:
         print("Player 2's turn")
-    board.draw(actualBoard)
+    board.draw()
     print("Select the piece you want to move and enter the coordinates to where you want to move it to")
     coordX = input()
     coordY = input()
