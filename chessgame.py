a3e1ca3b8abe19d341f3b54c4a70c4957b988d87
Nuddle <34684697+Nuddle@users.noreply.github.com>
2018-10-25 14:10:28 +0200

    Fixed loop

--- chessgame.py
+++ chessgame.py
@@ -4,8 +4,8 @@ board = Board()
 actualBoard = board.initialize()
 
 print("Hello and welcome to Chess !")
-
-while :
+end = False
+while end != True:
 
     board.draw(actualBoard)
     print("Select the piece you want to move and enter the coordinates to where you want to move it to")
