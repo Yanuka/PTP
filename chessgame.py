3a898d432a0703bcb35d56932449778704a79114
sorasgaming <32437365+sorasgaming@users.noreply.github.com>
2018-11-13 23:43:13 +0100

    Corrected input 2

--- chessgame.py
+++ chessgame.py
@@ -6,7 +6,6 @@ print("Game designed by Lucas, Maxime, Joseph, Alexandre & Morgan\n")
 input("Press enter to start the game...")
 print("\033[H\033[J") #Clears the board
 
-
 end = False
 while end != True: #Plays the game until the game is ended
 
@@ -23,6 +22,8 @@ while end != True: #Plays the game until the game is ended
             board.draw() #Updates the current board display
             print("The value you have entered is incorrect")
         elif selectedPiece[0].isalpha()==False or selectedPiece[1].isdigit()==False :
+            print("\033[H\033[J") #Clears the board
+            board.draw() #Updates the current board display
             print("The value you have entered is incorrect")
         else:
             correctInputPiece=True
@@ -31,8 +32,12 @@ while end != True: #Plays the game until the game is ended
     while correctInputCoord==False :
         selectedCoord = input("Select the square of where you want to move it to : ") #Gets the destination square
         if len(selectedCoord)!=2 :
+            print("\033[H\033[J") #Clears the board
+            board.draw() #Updates the current board display
             print("The value you have entered is incorrect")
         elif selectedCoord[0].isalpha()==False or selectedCoord[1].isdigit()==False :
+            print("\033[H\033[J") #Clears the board
+            board.draw() #Updates the current board display
             print("The value you have entered is incorrect")
         else:
             correctInputCoord=True
@@ -42,10 +47,5 @@ while end != True: #Plays the game until the game is ended
     selectedPieceX = int(selectedPiece[1]) - 1
     selectedCoordY = ord(selectedCoord[0]) - 65
     selectedCoordX = int(selectedCoord[1]) - 1
-
-    if board.currentPlayerIsOne == False:
-        board.currentPlayerIsOne = True
-    else:
-        board.currentPlayerIsOne = False
 
     print("\033[H\033[J") #Clears the board
