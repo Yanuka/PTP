86c2fbe31f82c6497910f3667785dae3760fad0c
Joseph <lloyd.joseph89@gmail.com>
2018-11-13 16:30:03 +0100

    Input change 1

--- chessgame.py
+++ chessgame.py
@@ -15,8 +15,11 @@ while end != True: #Plays the game until the game is ended
     board.draw() #Updates the current board display
     print()
     selectedPiece = input("Select the square of the piece you want to move : ") #Gets the square of the piece
+    if len(selectedPiece)>2 :
+        print("The value you have entered is incorrect")
     selectedCoord = input("Select the square to where you want to move the piece : ") #Gets the square of the coordinates
-
+    if len(selectedCoord)>2 :
+        print("The value you have entered is incorrect")
     selectedPieceX = ord(selectedPiece[0]) - 65 #Converts the input into coordinates
     selectedPieceY = int(selectedPiece[1]) - 1
     selectedCoordX = ord(selectedCoord[0]) - 65
